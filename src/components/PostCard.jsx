b4ee7b979b36a657dffddde1de2dc72ed8662a95
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2024-10-27 13:51:42 +0530

    hover problem

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -27,6 +27,31 @@
 
 
 
+// import React from 'react';
+// import appwriteService from "../appwrite/conf";
+// import { Link } from 'react-router-dom';
+
+// function PostCard({ $id, title, featuredImage }) {
+//     return (
+//         <Link to={`/post/${$id}`} className="w-full">
+//             <div className='bg-white shadow-md rounded-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-lg'>
+//                 <div className='w-full h-48 relative'>
+//                     <img 
+//                         src={appwriteService.getFilePreview(featuredImage)} 
+//                         alt={title} 
+//                         className='w-full h-full object-cover p-2 rounded-2xl' 
+//                     />
+//                 </div>
+//                 <div className='p-4'>
+//                     <h2 className='text-lg font-semibold text-gray-800 line-clamp-2'>{title}</h2>
+//                 </div>
+//             </div>
+//         </Link>
+//     );
+// }
+
+// export default PostCard;
+
 import React from 'react';
 import appwriteService from "../appwrite/conf";
 import { Link } from 'react-router-dom';
@@ -34,7 +59,7 @@ import { Link } from 'react-router-dom';
 function PostCard({ $id, title, featuredImage }) {
     return (
         <Link to={`/post/${$id}`} className="w-full">
-            <div className='bg-white shadow-md rounded-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-lg'>
+            <div className='bg-white shadow-md rounded-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-lg active:scale-100 active:shadow-none'>
                 <div className='w-full h-48 relative'>
                     <img 
                         src={appwriteService.getFilePreview(featuredImage)} 
@@ -51,5 +76,7 @@ function PostCard({ $id, title, featuredImage }) {
 }
 
 export default PostCard;
+
+
 
 
